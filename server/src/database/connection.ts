a6abb2d81f428fea17b3ecd7e6391d0fcd39316f
thiagomedina <thiagomedina001@gmail.com>
2020-08-04 21:45:04 -0300

    create migrations and post route

--- server/src/database/connection.ts
+++ server/src/database/connection.ts
@@ -1,5 +1,5 @@
 import knex from 'knex';
-import path from   'path'
+import path from  'path'
 
 const db = knex({
     client: 'sqlite3',
---
+++ server/src/routes.ts
@@ -0,0 +1,57 @@
+import express from "express";
+import db from "./database/connection";
+import convertHourToMinutes from "./utils/convertHourToMinutes";
+
+const routes = express.Router();
+
+interface ScheduleItem {
+  week_day: number;
+  from: string;
+  to: string;
+}
+
+routes.get("/classes", async (req, res) => {
+  const { name, avatar, whatsapp, bio, subject, cost, schedule } = req.body;
+
+  const trx = await db.transaction();
+
+  try {
+    const insertedUsersIds = await trx("users").insert({
+      name,
+      avatar,
+      whatsapp,
+      bio,
+    });
+
+    const user_id = insertedUsersIds[0];
+
+    const insertedClassesIds = await trx("classes").insert({
+      subject,
+      cost,
+      user_id,
+    });
+
+    const class_id = insertedClassesIds[0];
+    const classSchedule = schedule.map((item: ScheduleItem) => {
+      return {
+        class_id,
+        week_day: item.week_day,
+        from: convertHourToMinutes(item.from),
+        to: convertHourToMinutes(item.to),
+      };
+    });
+
+    await trx("class_schedule").insert(classSchedule);
+
+    await trx.commit();
+    return res.status(201).send();
+  } catch (err) {
+    await trx.rollback();
+    console.log(err);
+    return res.status(400).json({
+      error: "error while creating new class",
+    });
+  }
+});
+
+export default routes;
