6b14054c34629feb60198c0699d8492fc7518ddc
thiagomedina <thiagomedina001@gmail.com>
2020-08-05 17:27:53 -0300

    form styles

--- frontend-web/src/pages/TeacherForm/index.tsx
+++ frontend-web/src/pages/TeacherForm/index.tsx
@@ -1,11 +1,37 @@
-import React from 'react';
-import PageHeader from '../../components/PageHeader'
+import React from "react";
+import PageHeader from "../../components/PageHeader";
+import './styles.css'
 
+function TeacherForm() {
+  return (
+    <div id="page-teacher-form" className="container">
+      <PageHeader
+        title="Que incrível que você quer dar aulas"
+        description="O primeiro passo é preencher o formulário"
+      />
 
-function TeacherForm(){
-    return(
-       <PageHeader title="Que incrível que você quer dar aulas" />
-    );
+      <main>
+        <fieldset>
+          <legend>Seus dados </legend>
+
+          <div className="input-block">
+            <label htmlFor="name">Nome Completo</label>
+            <input type="text" id="name" />
+          </div>
+
+          <div className="input-block">
+            <label htmlFor="avatar">Foto</label>
+            <input type="text" id="avatar" />
+          </div>
+
+          <div className="input-block">
+            <label htmlFor="whatsapp">Whatsapp</label>
+            <input type="text" id="whatsapp" />
+          </div>
+        </fieldset>
+      </main>
+    </div>
+  );
 }
 
 export default TeacherForm;
