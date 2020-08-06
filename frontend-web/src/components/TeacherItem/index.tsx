821161e7fcef773bfe6c0c3480508c52b5448188
thiagomedina <thiagomedina001@gmail.com>
2020-08-05 21:51:35 -0300

    frontend

--- frontend-web/src/components/TeacherItem/index.tsx
+++ frontend-web/src/components/TeacherItem/index.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import whatsappIcon from "../../assets/images/icons/whatsapp.svg";
 import "./styles.css";
+import api from "../../services/api";
 
 export interface Teacher {
   id: number;
@@ -17,6 +18,12 @@ interface TeacherItemProps {
 }
 
 const TeacherItem: React.FC<TeacherItemProps> = ({ teacher }) => {
+
+  function createNewConnection(){
+    api.post('connections',{
+      user_id: teacher.id,
+    })
+  }
   return (
     <article className="teacher-item">
       <header>
@@ -34,7 +41,7 @@ const TeacherItem: React.FC<TeacherItemProps> = ({ teacher }) => {
           Preço/hora
           <strong>R$ {teacher.cost} </strong>
         </p>
-        <button type="button">
+        <button onclick={createNewConnection } type="button">
           <img src={whatsappIcon} alt="" />
           entrar em contato
         </button>
