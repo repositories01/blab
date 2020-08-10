98ec9d1520dfd4ee925b0f5e72ecbb30591eb532
thiagomedina <thiagomedina001@gmail.com>
2020-08-10 20:11:27 -0300

    login

---
+++ frontend-web/src/pages/SignIn/index.tsx
@@ -0,0 +1,8 @@
+import React from  'react';
+
+
+const SignIn: React.FC = () => {
+return <h1>login</h1>
+}
+
+export default SignIn;
--- frontend-web/src/routes.tsx
+++ frontend-web/src/routes.tsx
@@ -3,10 +3,12 @@ import { BrowserRouter, Route } from "react-router-dom";
 import Landing from "./pages/Landing";
 import TeacherList from "./pages/TeacherList";
 import TeacherForm from "./pages/TeacherForm";
+import SignIn from "./pages/SignIn";
 
 function Routes() {
   return (
     <BrowserRouter>
+      <Route path="/login"  component={SignIn}/>
       <Route path="/" exact component={Landing} />
       <Route path="/study" component={TeacherList} />
       <Route path="/give-classes" component={TeacherForm} />
