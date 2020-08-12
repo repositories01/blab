11684be56b84abea9a65f754a700968a717fd207
thiagomedina <thiagomedina001@gmail.com>
2020-08-12 13:12:00 -0300

    auth controller

--- backend-api/src/routes.ts
+++ backend-api/src/routes.ts
@@ -1,12 +1,12 @@
 import express from "express";
 import ClassesController from "./controllers/ClassesController";
 import ConnectionsController from "./controllers/ConnectionsController";
-import LoginController from "./controllers/LoginController";
+import AuthController from "./controllers/AuthController";
 
 const routes = express.Router();
 const classesController = new ClassesController();
 const connectionsController = new ConnectionsController();
-const loginController = new LoginController();
+const authController = new AuthController();
 
 routes.get("/classes", classesController.index);
 routes.post("/classes", classesController.create);
@@ -14,5 +14,5 @@ routes.post("/classes", classesController.create);
 routes.get("/connections", connectionsController.index);
 routes.post("/connections", connectionsController.create);
 
-routes.post("/login", loginController.auth);
+routes.post("/login", authController.auth);
 export default routes;
