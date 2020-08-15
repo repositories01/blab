bd6a6130eaf711e245de0bcb47e03deea980f5d3
dev <thiagomedina>
2020-08-15 19:33:17 -0300

    login method update

--- backend-api/src/controllers/AuthController.ts
+++ backend-api/src/controllers/AuthController.ts
@@ -1,14 +1,28 @@
 import { Request, Response } from "express";
 import crypto from "crypto";
 import db from "../database/connection";
-// import jwt from "jsonwebtoken";
 import * as jwt from '../utils/jwt'
 
+
 export default class AuthController {
+
+
   async login(req: Request, res: Response) {
+
+    // const [hashType, hash] = req.headers.authorization?.split(' ');
+    const parameters = req.headers.authorization?.split(' ');
+    const iterator = parameters[Symbol.iterator]();
+    const hashType = iterator.next().value
+    const hash = iterator.next().value
+
+    const credentials = Buffer.from(hash, 'base64').toString();
+    console.log(credentials)
+
     const users = await db("accounts");
     return res.status(200).json(users);
   }
+
+
 
   async signup(req: Request, res: Response) {
     const { name, email, password } = req.body;
@@ -24,11 +38,11 @@ export default class AuthController {
     try {
       if (user.length == 0) {
         await db("accounts").insert(userObj);
-        const userId:number = await db("accounts")
+        const userId: number = await db("accounts")
           .where({ email: email })
           .select("id");
 
-          const token = jwt.sign({ user: userId })
+        const token = jwt.sign({ user: userId })
 
         // const verify = jwt.verify(token, "a21za2FtbHNkYW1rbGRhc2Q");
         res.status(201).json({ userId, token });
