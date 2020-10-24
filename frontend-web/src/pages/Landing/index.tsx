43de3fa8b22b89972178a86da4c96baeac9489db
thiagomedina <thiagomedina001@gmail.com>
2020-10-24 20:46:53 -0300

    created hooks to auth

--- frontend-web/src/pages/Landing/index.tsx
+++ frontend-web/src/pages/Landing/index.tsx
@@ -1,64 +1,63 @@
-import React, { useState, useEffect } from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
 
-import logoImg from '../../assets/images/logo.png'
-import landingImg from '../../assets/images/landing.svg'
+import logoImg from "../../assets/images/logo.png";
+import landingImg from "../../assets/images/landing.svg";
 
-import studyIcon from '../../assets/images/icons/study.svg'
-import giveClassesIcon from '../../assets/images/icons/give-classes.svg'
-import purpleHeartIcon from '../../assets/images/icons/purple-heart.svg'
+import studyIcon from "../../assets/images/icons/study.svg";
+import giveClassesIcon from "../../assets/images/icons/give-classes.svg";
+import purpleHeartIcon from "../../assets/images/icons/purple-heart.svg";
 
-import api from '../../services/api';
+import api from "../../services/api";
 
-import './styles.css'
+import "./styles.css";
 
 function Landing() {
+  const [totalConnections, setTotalConnections] = useState(0);
 
-    const [totalConnections, setTotalConnections] = useState(0)
+  useEffect(() => {
+    api.get("connections").then((response) => {
+      const { total } = response.data;
 
-    useEffect(() => {
-        api.get('connections').then(response => {
-            const { total } = response.data;
+      setTotalConnections(total);
+    });
+  }, []);
 
-            setTotalConnections(total);
-        })
-    }, []);
+  return (
+    <div>
+      <div id="page-landing">
+        <div id="page-landing-content" className="container">
+          <div className="logo-container">
+            <img src={logoImg} alt="Proffy" />
+            <h2>Sua plataforma de estudos online.</h2>
+          </div>
 
-    return (
-        <div>
-        <div id="page-landing">
-            <div id="page-landing-content" className="container">
-                <div className="logo-container">
-                    <img src={logoImg} alt="Proffy"/>
-                    <h2>Sua plataforma de estudos online.</h2>
-                </div>
+          <img
+            src={landingImg}
+            alt="Plataforma de estudos"
+            className="hero-image"
+          />
 
-                <img src={landingImg} alt="Plataforma de estudos" className="hero-image"/>
-            
-                <div className="buttons-container">
-                    {/* <Link to="/study" className="study">
-                        <img src={studyIcon} alt="Estudar"/>
-                        Estudar
-                    </Link> */}
+          <div className="buttons-container">
+            <Link to="/study" className="study">
+              <img src={studyIcon} alt="Estudar" />
+              Estudar
+            </Link>
 
-                    <Link to="/give-classes" className="give-classes">
-                        <img src={giveClassesIcon} alt="Estudar"/>
-                       Como funciona
-                    </Link>
-                </div>
+            <Link to="/give-classes" className="give-classes">
+              <img src={giveClassesIcon} alt="Estudar" />
+              Como funciona
+            </Link>
+          </div>
 
-                <span className="total-connections">
-                    Total de { totalConnections } conexões já realizadas <img src={purpleHeartIcon} alt="Coração roxo" />
-                </span>
-            </div>
+          <span className="total-connections">
+            Total de {totalConnections} conexões já realizadas{" "}
+            <img src={purpleHeartIcon} alt="Coração roxo" />
+          </span>
         </div>
-
-        <div>
-
-            ssqsq
-        </div>
-        </div>
-    )
+      </div>
+    </div>
+  );
 }
 
 export default Landing;
