a77375e30a3c606c6942e9e8b5c031bcbd829dca
thiagomedina <thiagomedina001@gmail.com>
2020-11-14 21:42:50 -0300

    updated test

--- frontend-web/src/__tests__/pages/Landing.spec.tsx
+++ frontend-web/src/__tests__/pages/Landing.spec.tsx
@@ -1,11 +1,16 @@
 import React from "react";
+import { BrowserRouter as Router } from "react-router-dom";
 import Landing from "../../pages/Landing";
 import { render } from "@testing-library/react";
 
 describe("Landing page", () => {
   it(" should be able return the landing page", () => {
-    const { debug } = render(<Landing />);
-    
-    debug();
+    const { debug } = render(
+      <Router>
+        <Landing />
+      </Router>
+    );
+
+    debug()
   });
 });
