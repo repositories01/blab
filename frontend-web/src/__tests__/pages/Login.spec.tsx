2cf4e518db4fa6160c7b3fe6b8b2b8e236dbc8b8
thiagomedina <thiagomedina001@gmail.com>
2020-11-16 21:04:11 -0300

    update

--- frontend-web/src/__tests__/pages/Login.spec.tsx
+++ frontend-web/src/__tests__/pages/Login.spec.tsx
@@ -3,17 +3,17 @@ import { BrowserRouter as Router } from "react-router-dom";
 import Login from "../../pages/Login";
 import { render } from "@testing-library/react";
 
-
-jest.mock('react-router-dom', () => {
-    return{
-        useHistory: jest.fn(),
-        Link: ({children}:{children: React.ReactNode}) => children,
-    }
-})
+jest.mock("react-router-dom", () => {
+  return {
+    useHistory: jest.fn(),
+    Link: ({ children }: { children: React.ReactNode }) => children,
+  };
+});
 describe("Landing page", () => {
   it(" should be able to login", () => {
-    const { debug } = render(<Login />);
+    const { getByPlaceholderText } = render(<Login />);
 
-    debug();
+    const emailField = getByPlaceholderText("Email");
+    const passwordField = getByPlaceholderText("Senha");
   });
 });
