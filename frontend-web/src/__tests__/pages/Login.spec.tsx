10c2a0e48b9f28959822ca51320503b85923d8eb
thiagomedina <thiagomedina001@gmail.com>
2020-11-16 21:39:34 -0300

    init api mock

--- frontend-web/src/__tests__/pages/Login.spec.tsx
+++ frontend-web/src/__tests__/pages/Login.spec.tsx
@@ -3,17 +3,26 @@ import { BrowserRouter as Router } from "react-router-dom";
 import Login from "../../pages/Login";
 import { render, fireEvent } from "@testing-library/react";
 
+const mockedHistoryPush = jest.fn();
 jest.mock("react-router-dom", () => {
   return {
-    useHistory: jest.fn(),
+    useHistory: () => ({
+      push: mockedHistoryPush,
+    }),
     Link: ({ children }: { children: React.ReactNode }) => children,
   };
 });
 describe("Landing page", () => {
   it(" should be able to login", () => {
-    const { getByPlaceholderText } = render(<Login />);
+    const { getByPlaceholderText, getByText } = render(<Login />);
 
     const emailField = getByPlaceholderText("Email");
     const passwordField = getByPlaceholderText("Senha");
+    const buttonElement = getByText("Entrar");
+
+    fireEvent.change(emailField, { target: { value: "nome@email.com" } });
+    fireEvent.change(passwordField, { target: { value: "1212121" } });
+    fireEvent.click(buttonElement);
+    expect(mockedHistoryPush).toHaveBeenCalledWith("/give-classes");
   });
 });
