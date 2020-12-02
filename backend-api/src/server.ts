773e432531a80b825fd85a2583ef92748d979e65
thiagomedina <thiagomedina001@gmail.com>
2020-12-02 11:12:12 -0300

    log server

--- backend-api/src/server.ts
+++ backend-api/src/server.ts
@@ -10,4 +10,7 @@ app.use(express.json());
 
 app.use(routes);
 
-app.listen(3333);
+app.listen(3333, () => {
+    // eslint-disable-next-line
+    console.log('⚡️ Server started on port 3333!');
+  });
