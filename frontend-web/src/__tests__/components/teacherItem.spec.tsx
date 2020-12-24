4abcabea127ab0e84ba2e92bd72715a442882aa5
thiagomedina <thiagomedina001@gmail.com>
2020-12-23 21:03:00 -0300

    teacher item component

--- frontend-web/src/__tests__/components/teacherItem.spec.tsx
+++ frontend-web/src/__tests__/components/teacherItem.spec.tsx
@@ -3,3 +3,5 @@ import { BrowserRouter as Router } from "react-router-dom";
 import TeacherList from "../../pages/TeacherList";
 import TeacherItem from "../../components/TeacherItem";
 import { render, fireEvent, wait } from "@testing-library/react";
+
+
