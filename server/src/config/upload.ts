6f2c158616d8756d854b46f1e3a8ad3cb6eff074
thiagomedina <thiagomedina001@gmail.com>
2021-01-07 21:38:33 -0300

    added upload fuctionality

--- server/src/config/upload.ts
+++ server/src/config/upload.ts
@@ -1,6 +1,14 @@
+import path from 'path'
 import multer from 'multer'
-
+import crypto from 'crypto'
 
 export default {
-    storage: multer.diskStorage({})
+    storage: multer.diskStorage({
+        destination: path.resolve(__dirname, '..', '..', 'tmp'),
+        filename(request, file, callback) {
+            const filehash = crypto.randomBytes(10).toString("hex");
+            const filename = `${filehash}-${file.originalname}`
+            return callback(null, filename)
+        }
+    }),
 }
