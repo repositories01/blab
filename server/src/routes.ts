e77d6cec1e53ee13d5d2b6244dfbe0d5e28ccb78
thiagomedina <thiagomedina001@gmail.com>
2021-01-07 21:48:39 -0300

    upload to app

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -2,12 +2,13 @@ import express from "express";
 import ClassesController from "./controllers/ClassesController";
 import ConnectionsController from "./controllers/ConnectionsController";
 import AuthController from "./controllers/AuthController";
-
+import multer from 'multer'
+import uploadConfig from './config/upload'
 const routes = express.Router();
 const classesController = new ClassesController();
 const connectionsController = new ConnectionsController();
 const authController = new AuthController();
-
+const upload = multer(uploadConfig)
 
 
 routes.get("/classes", classesController.index);
@@ -21,7 +22,7 @@ routes.get("/login", authController.login);
 routes.post("/signup", authController.signup);
 routes.get('/index', authController.index);
 
-routes.patch('/avatar', (req, res) => {
+routes.patch('/avatar', upload.single('avatar'), (req, res) => {
     return res.json('ok')
 })
 
