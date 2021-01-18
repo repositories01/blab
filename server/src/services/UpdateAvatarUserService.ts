4b687e8f24dc17d00c6381c2f40c878c41ffc6a6
thiagomedina <thiagomedina001@gmail.com>
2021-01-17 21:32:51 -0300

    deleted the avatar case it exists

--- server/src/services/UpdateAvatarUserService.ts
+++ server/src/services/UpdateAvatarUserService.ts
@@ -1,5 +1,11 @@
 import db from "../database/connection";
 import { IUser } from '../controllers/AuthController'
+import path from 'path'
+import uploadConfig from "../config/upload";
+import fs from 'fs'
+
+
+
 interface Request {
    user_id: string | number;
    avatarFileName: string;
@@ -10,17 +16,23 @@ class UpdateUserAvatarService {
 
       const user: IUser[] = await db("users")
          .where({ id: user_id })
-       
+
 
       if (!user[0]) {
          throw new Error('only auth users can change the avatar')
       }
 
-      if(user[0].avatar){
-        // delet avatar
+      if (user[0].avatar) {
+
+         const userAvatarfilePath = path.join(uploadConfig.directory, user[0].avatar)
+         const userAvatarFileExists = await fs.promises.stat(userAvatarfilePath)
+
+         if (userAvatarFileExists) {
+            await fs.promises.unlink(userAvatarfilePath)
+         }
       }
 
-    
+
    }
 
 }
