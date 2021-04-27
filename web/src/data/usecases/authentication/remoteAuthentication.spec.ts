c3137880e8fa13bf9fc3ecb571ac118456f5eec4
thiagomedina <contato@thiagomedina.me>
2021-04-27 20:52:50 -0300

    refactor: created a factory to return the test object

--- web/src/data/usecases/authentication/remoteAuthentication.spec.ts
+++ web/src/data/usecases/authentication/remoteAuthentication.spec.ts
@@ -2,14 +2,28 @@ import RemoteAuthentication from './remoteAuthentication'
 import { HttpPostClientSpy } from '../../test/mockHttpClient'
 
 
+type SystemUnderTestTypes = {
+    systemUnderTest: RemoteAuthentication;
+    httpPostClientSpy: HttpPostClientSpy;
+}
+
+const makeUnderTest = (url: string = 'any_url'): SystemUnderTestTypes => {
+    const httpPostClientSpy = new HttpPostClientSpy()
+    const systemUnderTest = new RemoteAuthentication(url, httpPostClientSpy)
+
+    return {
+        httpPostClientSpy,
+        systemUnderTest
+    }
+}
+
 describe("RemoteAuthentication", () => {
 
     it("Should to call HttpPostClient with correct URL", async () => {
         const url = 'any_url';
-        const httpPostClient = new HttpPostClientSpy()
-        const systemUnderTest = new RemoteAuthentication(url, httpPostClient)
+        const { httpPostClientSpy, systemUnderTest } = makeUnderTest(url)
         await systemUnderTest.auth()
-        expect(httpPostClient.url).toBe(url)
+        expect(httpPostClientSpy.url).toBe(url)
 
     });
 });
