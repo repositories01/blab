b0a626a16d350aa065e90432be9f1512219e0727
thiagomedina <thiagomedina001@gmail.com>
2021-09-26 16:20:27 -0300

    emailValidator

--- server/src/presentation/controllers/signup.spec.ts
+++ server/src/presentation/controllers/signup.spec.ts
@@ -1,8 +1,22 @@
 import { SignUpController } from './signup'
+import { IEmailValidator } from '../protocols/email-validator'
+
+const makeSut = (): SignUpController => {
+
+  class EmailValidatorSub implements IEmailValidator {
+    isValid(email: string): boolean {
+      return true
+    }
+  }
+  const emailValidator = new EmailValidatorSub()
+  return new SignUpController(emailValidator)
+
+
+}
 
 describe('SignUp Controller', () => {
   it("should returns a 400 status code with name is no provider", () => {
-    const sut = new SignUpController()
+    const sut = makeSut()
 
     const httpRequest = {
       body: {
@@ -18,11 +32,27 @@ describe('SignUp Controller', () => {
   })
 
   it("should returns a 400 status code with name is no provider", () => {
-    const sut = new SignUpController()
+    const sut = makeSut()
 
     const httpRequest = {
       body: {
         name: 'any-name',
+        password: 'any-password',
+        passwordConfirm: 'any-password'
+      }
+    }
+    const httpResponse = sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toEqual(new Error('Missing params: email'))
+
+  })
+  it("should returns a 400 status code if invalid email is provided", () => {
+    const sut = makeSut()
+
+    const httpRequest = {
+      body: {
+        name: 'any-name',
+        email: 'invalidemail',
         password: 'any-password',
         passwordConfirm: 'any-password'
       }
--- server/src/presentation/controllers/signup.ts
+++ server/src/presentation/controllers/signup.ts
@@ -1,4 +1,11 @@
+import { IEmailValidator } from '../protocols/email-validator'
 export class SignUpController {
+  private readonly emailValidator: IEmailValidator
+
+  constructor(emailValidator: IEmailValidator) {
+    this.emailValidator = emailValidator
+  }
+
   handle(httpRequest: any): any {
 
     if (!httpRequest.body.name) {
