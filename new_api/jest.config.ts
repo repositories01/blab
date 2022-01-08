390cb35edcb4ac02e5fc70031fea6f7cd1202f17
thiagomedina <thiagomedina001@gmail.com>
2022-01-08 19:35:45 -0300

    chore: add jest and husky

--- new_api/jest.config.ts
+++ new_api/jest.config.ts
@@ -1,16 +1,15 @@
 /*
  * For a detailed explanation regarding each configuration property and type check, visit:
- * https://jestjs.io/docs/en/configuration.html
+ * https://jestjs.io/docs/configuration
  */
 
 export default {
   roots: ['<rootDir>/src'],
-  clearMocks: true,
-  collectCoverageFrom: ['<rootDir>/src/**/*.ts '],
+  collectCoverage: true,
+  collectCoverageFrom: ['<rootDir/src/**/*.ts>'],
   coverageDirectory: "coverage",
   coverageProvider: "v8",
-  testEnvironment: "node",
-  transform: {
+  transform:{
     '.+\\.ts$': 'ts-jest'
   },
 
