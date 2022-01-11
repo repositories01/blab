d4aa552b8f49b4239b21b9e07d6129b41d96d8b4
thiagomedina <thiagomedina001@gmail.com>
2022-01-11 19:06:45 -0300

    moved files

--- new_api/src/presentation/controllers/signup-controller.ts
+++ new_api/src/presentation/controllers/signup-controller.ts
@@ -2,7 +2,7 @@ import { HttpResponse, HttpRequest } from "../protocols/http"
 import { badRequest } from "../helpers/http-helper"
 import { MissingParamError } from "../errors/missing-param-error"
 import { Controller } from "../protocols/controller"
-import { EmailValidator } from "../email-validator"
+import { EmailValidator } from "../helpers/email-validator"
 import { InvalidParamError } from "../errors/invalid-param-error"
 
 export class SignUpController implements Controller{
